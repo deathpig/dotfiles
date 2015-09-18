e9affafa5bef328e0c220bb63a772cb952f3ac40
anthony <anthony.pan@scotiabank.com>
2015-09-18 16:52:52 -0400

    more location for project dir

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias proxy='export http_proxy=gateway.bns:8000;export https_proxy=gateway.bns:443'
 alias noproxy='export http_proxy=;export https_proxy='
+alias xcodebuild='xctool'
